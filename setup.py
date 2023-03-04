d78fd590755fcf600ef95ac76f60c24fdafbf5af
ikonrafe <48784271+ikonrafe@users.noreply.github.com>
2023-03-04 22:49:51 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import requests
+
+def locate_phone(api_key, 0896-5302-8380):
+    url = f'https://www.googleapis.com/geolocation/v1/geolocate?key={api_key}'
+    headers = {'content-type': 'application/json'}
+    data = {'considerIp': 'false',
+            'wifiAccessPoints': [],
+            'cellTowers': [{'cellId': 42, 'locationAreaCode': 021, 'mobileCountryCode': 510, 'mobileNetworkCode': 11}]}
+
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 200:
+        location = response.json()['location']
+        accuracy = response.json()['accuracy']
+        print(f'Phone {phone_number} is at ({location["lat"]}, {location["lng"]}), accuracy: {accuracy} meters')
+    else:
+        print('Unable to locate phone')
+
+# contoh penggunaan
+api_key = 'YOUR_API_KEY'
+phone_number = '555-1234'
+locate_phone(api_key, phone_number)
