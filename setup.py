72c0b1aefbc4a24941da98ed8de5d94efd7ed0de
ikonrafe <48784271+ikonrafe@users.noreply.github.com>
2023-03-05 01:22:42 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,52 @@
 import requests
-api_key = AIzaSyAuUqm6atdaFjkBmo3k9pge86qH31am1FU
 
-phone_number = 6289653028380
-def locate_phone(api_key,phone_number):
+api_key = 'AIzaSyAuUqm6atdaFjkBmo3k9pge86qH31am1FU'
+
+phone_number = '6289653028380'
+
+def locate_phone(api_key, phone_number):
+
     url = f'https://www.googleapis.com/geolocation/v1/geolocate?key={api_key}'
+
     headers = {'content-type': 'application/json'}
-    data = {'considerIp': 'false',
-            'wifiAccessPoints': [],
-            'cellTowers': [{'cellId': 42, 'locationAreaCode': 21, 'mobileCountryCode': 510, 'mobileNetworkCode': 11}]}
+
+    data = {
+
+        'considerIp': 'false',
+
+        'wifiAccessPoints': [],
+
+        'cellTowers': [{
+
+            'cellId': 42,
+
+            'locationAreaCode': 21,
+
+            'mobileCountryCode': 510,
+
+            'mobileNetworkCode': 11
+
+        }]
+
+    }
 
     response = requests.post(url, headers=headers, json=data)
+
     if response.status_code == 200:
+
         location = response.json()['location']
+
         accuracy = response.json()['accuracy']
+
         print(f'Phone {phone_number} is at ({location["lat"]}, {location["lng"]}), accuracy: {accuracy} meters')
+
     else:
+
         print('Unable to locate phone')
 
 # contoh penggunaan
+
+locate_phone(api_key, phone_number)
 
 
 
